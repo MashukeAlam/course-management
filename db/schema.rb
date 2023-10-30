0aafd450df3db3882c15b829e17606f98b27ecd7
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-30 12:44:01 +0600

    home page added.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_30_063740) do
+ActiveRecord::Schema.define(version: 2023_10_30_064041) do
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "role"
