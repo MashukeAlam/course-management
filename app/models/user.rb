0c16006f66e2b0420f8f9baf18470dce38e905e7
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-31 17:00:06 +0600

    subjects_taken function added to send list of subjects which are a student is enrolled in

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,4 +46,9 @@ class User < ApplicationRecord
 
     return "You are in #{semester} of #{year}"
   end
+
+  def subjects_taken
+    subject_list = SemesterSubject.where(semester_id: (StudentProgress.find_by_user_id(2).semester_id))
+    return subject_list
+  end
 end
