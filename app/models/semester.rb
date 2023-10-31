38d6047a6a6f8c24037350c3d786c266ffb74aac
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-31 16:49:07 +0600

    fixed a bug in the method for number_of_subjects in semester.rb file.

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -2,7 +2,7 @@ class Semester < ApplicationRecord
   belongs_to :year
 
   def number_of_subjects
-    res = SemesterSubject.find_by_semester_id(id)
+    res = SemesterSubject.where(semester_id: id)
     if res == nil
       return 0
     end
